d22edba2ad2dd1d55f84279787bc5a76559f45b9
SANADA Euki <yuu.mat.930@gmail.com>
2026-07-09 13:03:09 +0900

    refactor: modify about termination criteria

--- src/numerical_analysis/differential_equation/foward_euler_method.py
+++ src/numerical_analysis/differential_equation/foward_euler_method.py
@@ -3,17 +3,17 @@ from typing import Callable, TypeAlias
 fn: TypeAlias = Callable[[float, float], float]
 
 
-def generateSolver(fn: fn, t: float, y: float, h: float = 0.1, steps: int = 10):
-    for _ in range(steps):
+def generateSolver(fn: fn, t: float, y: float, max: float, h: float = 0.1):
+    while t < max:
         gradient = fn(t, y)
         y = y + h * gradient
-        t += h
         yield t, y
+        t += h
 
 
 def main():
     print("y(t)の関数値")
-    for t, y in generateSolver(lambda _, y: -y, 0, 1):
+    for t, y in generateSolver(lambda _, y: -y, 0, 1, 1):
         print(f"{t:3f}, {y:3f}")
     # この微分方程式は dy/dt = -y であり、初期条件 y(0) = 1。特殊解は y(t) = e^(-t)。
 
