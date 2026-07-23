3434bddd67c4f13f72cb2a86dff60adffdc77731
SANADA Euki <yuu.mat.930@gmail.com>
2026-07-23 20:47:16 +0900

    feat: implement processing of multiplication, division and modulo

--- src/algo/chapters/chapter_04_stacks_queues/rpn.py
+++ src/algo/chapters/chapter_04_stacks_queues/rpn.py
@@ -1,17 +1,24 @@
 def main():
-    # expression = "5+4*4-2^4%3/1"
-    expression = "5+4"
+    expression = "5+4*4-2*4%3/1"
     postfix: list[str] = []
     stack: list[str] = []
 
     for c in expression:
+        print(stack)
         match c:
             case c if c.isdigit():
                 postfix.append(c)
             case c if c in "+-":
+                while stack and stack[-1] in "+-*/%^":
+                    postfix.append(stack.pop())
+                stack.append(c)
+            case c if c in "*/%":
+                while stack and stack[-1] in "*/%^":
+                    postfix.append(stack.pop())
                 stack.append(c)
             case _:
                 raise ValueError(f"Invalid character: {c}")
+                break
     else:
         if stack:
             postfix.append(stack.pop())
